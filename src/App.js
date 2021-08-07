7a29b10575690a3b39ad52d1a52bda928ac3abc2
Shelton Rolle <sheltonrolle@Sheltons-MacBook-Pro-2.local>
2021-08-07 17:59:16 -0400

    Created the Header component and started working on styling the search component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ function App() {
   return (
     <>
       <Header />
+      <div className="content-container">
+        <div className="search">
+          <form action="/search" method="POST">
+            <input type="text" name="query" placeholder="Search" />
+            <input type="submit" value="search" />
+          </form>
+        </div>
+      </div>
     </>
   );
 }
